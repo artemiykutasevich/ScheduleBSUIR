2aac0e25190355382cf445f89f34fde388699a04
Artem Kutasevich <artemiy.kutasevich@gmail.com>
2022-03-27 13:12:27 +0300

    Initial Commit

---
+++ ScheduleBSUIR/ScheduleBSUIRApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleBSUIRApp.swift
+//  ScheduleBSUIR
+//
+//  Created by Artem Kutasevich on 27.03.22.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleBSUIRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
